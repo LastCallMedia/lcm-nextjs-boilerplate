3dd23d6731b8a2e4d84b51ad0c1c7a54ba3611f3
Raiyan Ahmed <raiyan@lastcallmedia.com>
2025-07-14 23:46:44 +0600

    remove script

--- scripts/start-server.cjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-// Force IPv4 for Docker/act environments
-process.env.HOST = '0.0.0.0';
-process.env.HOSTNAME = '0.0.0.0';
-
-// Import and start the Next.js standalone server
-require('.next/standalone/server.js');
