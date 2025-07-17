30a7b74791237f791d22cd25af4a3605fb1aebad
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2025-07-17 08:55:19 -0700

    setup copilotkit and popover ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ import { Toaster } from "~/_components/ui/sonner";
 import Navbar from "~/_components/layout/Navbar";
 import { SessionProvider } from "next-auth/react";
 import { Footer } from "~/_components";
+import { CopilotKit } from "@copilotkit/react-core";
+import "@copilotkit/react-ui/styles.css";
+import { CopilotPopup } from "@copilotkit/react-ui";
 
 export const metadata: Metadata = {
   title: "Create LCM App",
@@ -27,19 +30,30 @@ export default function RootLayout({
     <html lang="en" className={`${geist.variable}`}>
       <body>
         <SessionProvider>
-          <TRPCReactProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <Navbar />
-              {children}
-              <Footer />
-              <Toaster />
-            </ThemeProvider>
-          </TRPCReactProvider>
+          <CopilotKit publicApiKey={process.env.NEXT_PUBLIC_COPILOTKIT_API_KEY}>
+            <TRPCReactProvider>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+              >
+                <Navbar />
+                {children}
+                <Footer />
+                <CopilotPopup
+                  instructions={
+                    "You are assisting the user as best as you can. Answer in the best way possible given the data you have."
+                  }
+                  labels={{
+                    title: "YetiBot",
+                    initial: "Need any help?",
+                  }}
+                />
+                <Toaster />
+              </ThemeProvider>
+            </TRPCReactProvider>
+          </CopilotKit>
         </SessionProvider>
       </body>
     </html>
