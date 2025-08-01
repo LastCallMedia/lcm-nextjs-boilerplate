56ad9adc7671857be0be3af54572b7dfd20173b5
nazmul8842@gmail.com <nazmul8842@gmail.com>
2025-08-01 23:27:15 +0600

    Refactor page components to use server-side rendering and improve authentication handling

--- src/app/[locale]/posts/example-csr/page.tsx
+++ src/app/[locale]/posts/example-csr/page.tsx
@@ -1,41 +1,33 @@
-"use client";
-
 import { TypingIndicator } from "~/_components/posts/TypingIndicator";
-import { api } from "~/trpc/react";
+import { api } from "~/trpc/server";
 import NextRenderingDocsLink from "~/app/[locale]/posts/_components/next-docs-link";
 import AllPostsClient from "~/app/[locale]/posts/client";
-import { FormattedMessage } from "react-intl";
+import { getMessages } from "~/i18n";
+import { auth } from "~/server/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  // CSR: fetch posts on render, and update when the query is invalidated. Loading and error handled right in the UI
-  const { data: posts, isLoading, isError, error } = api.post.getAll.useQuery();
-
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const session = await auth();
+  const { locale } = await params;
+  if (!session?.user) {
+    redirect(`/login?callbackUrl=/${locale}/posts/example-csr`);
+  }
+  // SSR: fetch posts on the server, only update on refresh. Loading and error handled by NextJS in ./loading.tsx and ./error.tsx
+  const posts = await api.post.getAll();
+  const messages = getMessages((locale || "en") as "en" | "es");
   return (
     <div className="m-auto flex max-w-2xl flex-col gap-8">
       <h1 className="m-4 text-center text-2xl font-bold">
-        <FormattedMessage id="posts.examples.csrPage.title" />
+        {messages["posts.examples.csrPage.title"]}
       </h1>
-      <p>
-        <FormattedMessage id="posts.examples.csrPage.description1" />
-      </p>
-      <p>
-        <FormattedMessage id="posts.examples.csrPage.description2" />
-      </p>
+      <p>{messages["posts.examples.csrPage.description1"]}</p>
+      <p>{messages["posts.examples.csrPage.description2"]}</p>
       <NextRenderingDocsLink />
-
-      {isLoading ? (
-        <div className="mb-4 text-center text-gray-500">
-          <FormattedMessage id="posts.loading" />
-        </div>
-      ) : isError ? (
-        <div className="mb-4 text-center text-red-500">
-          <FormattedMessage id="posts.error" />{" "}
-          {error?.message || "Unknown error"}
-        </div>
-      ) : (
-        <AllPostsClient posts={posts ?? []} />
-      )}
-
+      <AllPostsClient posts={posts} />
       <TypingIndicator channelId="landing" />
     </div>
   );
