c4701f72c5c65952cb64bb54c9734d252dc4a5e1
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2025-08-05 10:12:52 -0700

    add about and contact links to footer

--- src/app/[locale]/(public)/contact/page.tsx
+++ src/app/[locale]/(public)/contact/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function ContactPage() {
   return (
-    <div className="bg-background relative isolate pb-10">
+    <div className="bg-background relative isolate h-full">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative px-6 pt-24 pb-20 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
